a5bae569d865f3eab9ec0f93e9395adb0fd54ca5
ChinWanYoke <chinwy@tarc.edu.my>
2025-02-05 16:49:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-import pandas as pd
-import numpy as np
 import ast
+import pandas as pd
 import streamlit as st
+from openai.embeddings_utils import cosine_similarity
 import openai
 
-openai.api_key =  st.secrets["mykey"]
+# Set your OpenAI API key
+openai.api_key = st.secrets["mykey"] 
 
 df = pd.read_csv("qa_dataset_with_embeddings.csv")
+
+# Convert the string embeddings back to lists
 df['Question_Embedding'] = df['Question_Embedding'].apply(ast.literal_eval)
+
+def get_embedding(text, model="text-embedding-ada-002"):
+   return openai.Embedding.create(input = [text], model=model)['data'][0]['embedding']
 
 def find_best_answer(user_question):
    # Get embedding for the user's question
@@ -21,7 +27,7 @@ def find_best_answer(user_question):
    max_similarity = df['Similarity'].max()
 
    # Set a similarity threshold to determine if a question is relevant enough
-   similarity_threshold = 0.6  # You can adjust this value
+   similarity_threshold = 0.75  # You can adjust this value
 
    if max_similarity >= similarity_threshold:
       best_answer = df.loc[most_similar_index, 'Answer']
@@ -29,9 +35,18 @@ def find_best_answer(user_question):
    else:
       return "I apologize, but I don't have information on that topic yet. Could you please ask other questions?"
 
-# Streamlit UI
-st.title("FAQ for Heart, Lung, and Blood Related Health")
 
-product_name = st.text_input("Enter your questions:", value="Type here")
-if st.button("Submit"):
-    st.write(best_answer)
+# Streamlit Interface
+st.title("Smart FAQ Assistant (Heart, Lung, Blood Health)")
+
+user_question = st.text_input("Ask a question","Who will have Cardiomyopathy?")
+search_button = st.button("Find Answer")
+
+if search_button:
+    if not user_question:
+        st.warning("Please enter a question.")
+    else:
+        with st.spinner("Searching for the best answer..."):  # Display a spinner while searching
+            answer = find_best_answer(user_question)
+            st.write("## Answer:")
+            st.write(answer)
